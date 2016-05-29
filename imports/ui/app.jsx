80002a321cd8ce2f3c273ea08e684f0f55ef9c83
Marc Nitzsche <marc.nitzsche@live.de>
2016-05-29 17:52:21 +0200

    Create basic application structure and remove defaults

---
+++ imports/ui/app.jsx
@@ -0,0 +1,24 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
+
+// App component - represents the whole app
+class App extends Component {
+  constructor(props, context) {
+      super(props, context);
+    };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+};
+
+export default createContainer(() => ({}), App);
